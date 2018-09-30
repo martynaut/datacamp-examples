a5e5cb87fd835caf094c2646e98c9bc9f0937a1b
science-is-the-new-black <scienceisthenewblack@gmail.com>
2018-09-30 08:03:49 +0200

    keras example

--- keras_sequential_example.py
+++ keras_sequential_example.py
@@ -1,1 +1,115 @@
+from keras.layers import Dense
 from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping
+from keras.utils import to_categorical
+
+import matplotlib.pyplot as plt
+
+
+def get_new_model(input_shape=1):
+    model_i = Sequential()
+    model_i.add(Dense(100, activation='relu', input_shape=input_shape))
+    model_i.add(Dense(100, activation='relu'))
+    model_i.add(Dense(2, activation='softmax'))
+    return model_i
+
+
+def relu(input_value):
+    # Calculate the value for the output of the relu function: output
+    output = max(input_value, 0)
+
+    # Return the value just calculated
+    return output
+
+
+# Save the number of columns in predictors: n_cols
+n_cols = df.drop('output', axis=1).shape[1]
+
+y = to_categorical(df['output'])
+
+# Create the model: model
+model = Sequential()
+
+# Add the first hidden layer
+model.add(Dense(50, activation='relu', input_shape=(n_cols,)))
+
+# Add the second hidden layer
+model.add(Dense(50, activation='relu'))
+
+# Add the output layer
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Verify that model contains information from compiling
+print("Loss function: " + model.loss)
+
+early_stopping_monitor = EarlyStopping(patience=2)
+
+
+# Fit the model
+model.fit(predictors, target, epochs=30,
+          validation_split=0.3, callbacks=[early_stopping_monitor])
+
+# Calculate predictions: predictions
+predictions = model.predict(pred_data)
+
+# Calculate predicted probability of survival: predicted_prob_true
+predicted_prob_true = predictions[:, 1]
+
+# print predicted_prob_true
+print(predicted_prob_true)
+
+# Create list of learning rates: lr_to_test
+lr_to_test = [.000001, 0.01, 1]
+
+# Loop over learning rates
+for lr in lr_to_test:
+    print('\n\nTesting model with learning rate: %f\n' % lr)
+
+    # Build new model to test, unaffected by previous models
+    model = get_new_model()
+
+    # Create SGD optimizer with specified learning rate: my_optimizer
+    my_optimizer = SGD(lr=lr)
+
+    # Compile the model
+    model.compile(optimizer=my_optimizer, loss='categorical_crossentropy')
+
+    # Fit the model
+    model.fit(predictors, target)
+
+# Define early_stopping_monitor
+early_stopping_monitor = EarlyStopping(patience=2)
+
+# Create the new model: model_2
+model_2 = Sequential()
+
+# Add the first and second layers
+model_2.add(Dense(100, activation='relu', input_shape=input_shape))
+model_2.add(Dense(100, activation='relu'))
+
+# Add the output layer
+model_2.add(Dense(2, activation='softmax'))
+
+# Compile model_2
+model_2.compile(optimizer='adam', loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+# Fit model_1
+model_1_training = model_1.fit(predictors, target, epochs=15,
+                               validation_split=0.2, callbacks=[early_stopping_monitor],
+                               verbose=False)
+
+# Fit model_2
+model_2_training = model_2.fit(predictors, target, epochs=15, validation_split=0.2,
+                               callbacks=[early_stopping_monitor], verbose=False)
+
+# Create the plot
+plt.plot(model_1_training.history['val_loss'], 'r', model_2_training.history['val_loss'], 'b')
+plt.xlabel('Epochs')
+plt.ylabel('Validation score')
+plt.show()
